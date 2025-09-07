f0eafeb1acdac994ce487b23d085fd40f3e8dbb4
wata-nana <tonarinotototro@gmail.com>
2025-09-07 13:42:53 +0900

    gunicorn testn

--- app.py
+++ app.py
@@ -12,56 +12,56 @@ from google_oauth import auth_bp
 from routes import routes_bp, api_bp
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+# def create_app():
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    # instance ディレクトリがなければ必ず作成
-    os.makedirs(INSTANCE_DIR, exist_ok=True)
+# instance ディレクトリがなければ必ず作成
+os.makedirs(INSTANCE_DIR, exist_ok=True)
 
-    # DBの初期化
-    db.init_app(app)
-    # CLIからテーブル管理
-    Migrate(app, db)
-    """
-    新しいカラムを追加したい場合
-    flask db migrate -m "add カラム名 to user"
-    flask db upgrade
-    """
+# DBの初期化
+db.init_app(app)
+# CLIからテーブル管理
+Migrate(app, db)
+"""
+新しいカラムを追加したい場合
+flask db migrate -m "add カラム名 to user"
+flask db upgrade
+"""
 
-    # OAuthとFlask接続
-    oauth.init_app(app)
+# OAuthとFlask接続
+oauth.init_app(app)
 
-    # googleクライアント情報登録
-    oauth.register(
-        name="google",
-        client_id=app.config["GOOGLE_CLIENT_ID"],
-        client_secret=app.config["GOOGLE_CLIENT_SECRET"],
-        server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-        client_kwargs={"scope": "openid email profile"},
-    )
+# googleクライアント情報登録
+oauth.register(
+    name="google",
+    client_id=app.config["GOOGLE_CLIENT_ID"],
+    client_secret=app.config["GOOGLE_CLIENT_SECRET"],
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_kwargs={"scope": "openid email profile"},
+)
 
-    # Flask-Sessionの初期化
-    os.makedirs(app.config["SESSION_FILE_DIR"], exist_ok=True)
-    Session(app)
+# Flask-Sessionの初期化
+os.makedirs(app.config["SESSION_FILE_DIR"], exist_ok=True)
+Session(app)
 
-    # Flask-Login 初期化
-    login_manager = LoginManager(app)
-    login_manager.login_view = "routes.welcome"
+# Flask-Login 初期化
+login_manager = LoginManager(app)
+login_manager.login_view = "routes.welcome"
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return db.session.get(User, int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
 
-    # Blueprintの登録
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(routes_bp)
-    app.register_blueprint(api_bp, url_prefix="/api")
+# Blueprintの登録
+app.register_blueprint(auth_bp)
+app.register_blueprint(routes_bp)
+app.register_blueprint(api_bp, url_prefix="/api")
 
-    return app
+    # return app
 
 
 if __name__ == "__main__":
-    app = create_app()
+    # app = create_app()
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
