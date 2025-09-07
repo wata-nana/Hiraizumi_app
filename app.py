28ba6bf6d70c4495bc612e05633c0803518beb76
wata-nana <tonarinotototro@gmail.com>
2025-09-07 13:18:16 +0900

    ポート番号追加

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
